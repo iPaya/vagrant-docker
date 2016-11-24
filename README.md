0353601ae172182bc57bc347f8b3d07244402e4c
Di Zhang <zhangdi_me@163.com>
2016-11-24 10:18:34 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# vagrant-docker
-The vagrant docker environment.
+# Vagrant Docker
+基于 Vagrant 的 Docker 环境.
+
+## 用法
+
+### 1. 安装 Vagrant
+已安装的可以略过。
+
+参见 <https://www.vagrantup.com/docs/installation/>
+
+### 2. 下载代码
+
+```bash
+git clone https://github.com/iPaya/vagrant-docker.git
+```
+
+### 3. 运行
+```bash
+cd vagrant-docker && vagrant up
+```
+
+### 4. 打包并添加到 box
+```bash
+vagrant package && vagrant box add aPaya/docker package.box
+```
---
+++ provision/once-as-root.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#== Bash helpers ==
+
+function info {
+  echo " "
+  echo "--> $1"
+  echo " "
+}
+
+info "Provision-script user: `whoami`"
+
+info "Configure Apt Sources List"
+echo "deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+" > /etc/apt/sources.list
+echo "Done!"
+
+info "Install Docker"
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
+usermod -aG docker vagrant
+echo "Done!"
