3e4b523952842cd5aeb1ec682dfc958951d96479
张迪 <zhangdi_me@163.com>
2017-05-16 12:56:59 +0800

    Update once-as-root.sh

--- provision/once-as-root.sh
+++ provision/once-as-root.sh
@@ -11,10 +11,10 @@ function info {
 info "Provision-script user: `whoami`"
 
 info "Configure Apt Sources List"
-echo "deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
 " > /etc/apt/sources.list
 echo "Done!"
 
